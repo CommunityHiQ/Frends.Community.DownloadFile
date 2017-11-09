c72d565ab9c9e2e76b0a7a6cecb371865d8e7c5a
Ossi Galkin <ossi.galkin@hiq.fi>
2017-11-09 12:11:42 +0200

    Removed unnecessary imports

--- Frends.Community.WebFilesTests/DownloadTests.cs
+++ Frends.Community.WebFilesTests/DownloadTests.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace Frends.Community.Web.Tests
 {
     [TestClass()]
     public class DownloadTests
     {
+        // No test here because build server doesn't allow outgoing connections. 
     }
 }
